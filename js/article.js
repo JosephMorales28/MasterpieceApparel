1a49d09813ccfe462bdabd544a30e2ba4b864b4b
JosephMorales28 <josephmo2893@gmail.com>
2026-07-11 09:43:33 +0800

    mens wear

--- js/article.js
+++ js/article.js
@@ -1,34 +1,9 @@
 export function exp_article(){
     if(window.location.pathname==="/"|| window.location.pathname.endsWith("index.html")){
-       const title="Milestones";
-       const milestone=[
-                        {
-                            fb_followers:28,
-                            rb_followers:2,
-                            fb_views:2120,
-                            rb_favorites:10
-                        }
-       ];
-       const article_view=milestone.map(milestones =>`
-             <div class="milestone">
-                 <div>
-                     <p><span class= font-size-9>${milestones.fb_followers}</span></br>Fb Followers</p>
-                 </div>
-                 <div>
-                     <p><span class= font-size-9>${milestones.fb_views}</span></br>Fb Views</p>
-                 </div>
-                 <div>
-                     <p><span class= font-size-9>${milestones.rb_followers}</span></br>RB Followers</p>
-                 </div>
-                 <div>
-                     <p><span class= font-size-9>${milestones.rb_favorites}</span></br>RB Favorites</p>
-                 </div>
-             </div>`
-       ).join("");
+       const title="Mens Wear";
        return`
             <article>
                <h1>${title}</h1>
-               ${article_view}
             </article> 
             `;
     }
