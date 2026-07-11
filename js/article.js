1c2b94de75985b1118d5f75a535eff72e4a3dcd5
JosephMorales28 <josephmo2893@gmail.com>
2026-07-11 14:32:15 +0800

    image

--- js/article.js
+++ js/article.js
@@ -1,9 +1,48 @@
 export function exp_article(){
     if(window.location.pathname==="/"|| window.location.pathname.endsWith("index.html")){
        const title="Mens Wear";
+       const menImg={};
        return`
             <article>
                <h1>${title}</h1>
+               <div class="menwear">
+                        
+                        <picture>
+                             <img src="/img/menwear.webp" alt="classic tshirt" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2> I don't fish for food I fish for vibes</h2>
+                                 <button>View Full Details</button> <button class="btnred">Add on Redbubble Cart</button>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/menwear.webp" alt="classic tshirt" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2> I don't fish for food I fish for vibes</h2>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/menwear.webp" alt="classic tshirt" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2> I don't fish for food I fish for vibes</h2>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/menwear.webp" alt="classic tshirt" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2> I don't fish for food I fish for vibes</h2>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/menwear.webp" alt="classic tshirt" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2> I don't fish for food I fish for vibes</h2>
+                             </div>
+                        </picture>
+               </div>
             </article> 
             `;
     }
