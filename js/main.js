b6ae28db1cb96b087e269495c53ffd2441faba3e
JosephMorales28 <josephmo2893@gmail.com>
2026-07-13 15:49:08 +0800

    carousell scroll in main tag

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@ export function exp_main(){
         const main_hero='Get our new shirt release and buy now on masterpiece apparel and enjoy discount.';
         return`
               <main>
+              <div>
+                   <ul>
+                      <li>July 13, 2026</li>
+                      <li>New shirt release today!</li>
+                      <li>Masterpiece-Bunny the Striker (Essential Shirt)</li>
+                      <li>Only at $30.69</li>
+                      <li>Enjoy Discount 20%-30% Off until July 15, 2026</li>
+                      <li>Order and Buy now at Redbubble Store</li>
+                      <li>Follow us on Facebook and Instagram -Facebook: facebook.com/masterpieceapparelbyjoseph  -Instagram: instagram.com/masterpieceapparelbyjoseph</li>
+                      
+                      <li>July 13, 2026</li>
+                      <li>New shirt release today!</li>
+                      <li>Masterpiece-Bunny the Striker (Essential Shirt)</li>
+                      <li>Only $30.69</li>
+                      <li>Enjoy Discount 20%-30% Off until July 15, 2026</li>
+                      <li>Order and Buy now at Redbubble Store</li>
+                      <li>Follow us on Facebook and Instagram -Facebook: facebook.com/masterpieceapparelbyjoseph  -Instagram: instagram.com/masterpieceapparelbyjoseph</li>
+                   </ul> 
+              </div>
               <h1>${main_hero}</h1>
               </main>
         `;
