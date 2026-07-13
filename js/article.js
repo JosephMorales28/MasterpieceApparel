e7f7beee9bc8a8f3fb7569cce6f031700456a1d5
JosephMorales28 <josephmo2893@gmail.com>
2026-07-13 18:46:14 +0800

    on process in image tag array js

--- js/article.js
+++ js/article.js
@@ -2,20 +2,42 @@ export function exp_article(){
     if(window.location.pathname==="/"|| window.location.pathname.endsWith("index.html")){
        const title="Mens Wear";
        const creator="by: Joseph Morales";
-       const menImg=[
+       /*--const menImg=[
                      {
                         image:"/img/menwear.webp",
                         alt="classic shirt",
                         loading="lazy",
-                        priority:"high"
+                        fetchpriority:"high"
                      },
                      {
                         image:"/img/menwear2.webp",
                         alt="classic shirt",
                         loading="lazy",
-                        priority="high"
-                     }
-       ];
+                        fetchpriority="high"
+                     },
+                     {
+                        image:"/img/menwear3.webp",
+                        alt="classic shirt",
+                        loading="lazy",
+                        fetchpriority="high",
+                        title="I Don't fish for food I fish for vibes"
+                     },
+                     {
+                        image:"/img/menwear4.webp",
+                        alt="classic shirt",
+                        loading="lazy",
+                        fetchpriority="high"
+                        title="I Don't fish for food I fish for vibes"
+                     },
+       ];--*/
+       /*--const imgtag=menImg.filter(item=>item.fetchpriority==="high").map(item=>
+                                    `
+                                      <picture>
+                                            <img src="${item.image} alt="${item.alt}" loading="${item.loading}" fetchpriority="${item.fetchpriority}">
+                                            </picture>
+                                            <h2>${item.title}</h2>
+                                    `).join("");
+       */
        return`
             <article>
                <h1>${title}</h1>
