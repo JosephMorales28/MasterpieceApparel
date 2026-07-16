269cc9b0c7c5ff009b238213d97513681db6fda3
JosephMorales28 <josephmo2893@gmail.com>
2026-07-16 18:17:42 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'public', // 👈 tells Vite to use /public as the root
   build: {
-    outDir: 'dist'
-  }
-})
+    outDir: 'dist', // 👈 Vercel will deploy from /dist
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  },
+});
