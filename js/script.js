a907dc8c9bab7b4795b5955a84882a40cdde326b
JosephMorales28 <josephmo2893@gmail.com>
2026-07-16 18:09:54 +0800

    fixed

--- js/script.js
+++
@@ -1,7 +0,0 @@
-import {exp_header} from '../js/header.js';
-import {exp_main} from './main.js';
-import { exp_article } from './article.js';
-import {exp_aside} from './aside.js';
-import { exp_footer } from './footer.js';
-
-document.getElementById('app').innerHTML=exp_header()+exp_main()+exp_article()+exp_aside()+exp_footer();
