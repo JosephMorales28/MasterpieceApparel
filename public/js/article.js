428a682e522b6c32bfa303f1e80f72dc5f732f34
JosephMorales28 <josephmo2893@gmail.com>
2026-07-16 17:58:41 +0800

    copy css and js to public folder

---
+++ public/js/article.js
@@ -0,0 +1,138 @@
+export function exp_article(){
+    if(window.location.pathname==="/"|| window.location.pathname.endsWith("index.html")){
+       const title="Unisex Wear";
+       const creator="by: Joseph Morales";
+       /*--const menImg=[
+                     {
+                        image:"/img/menwear.webp",
+                        alt="classic shirt",
+                        loading="lazy",
+                        fetchpriority:"high"
+                     },
+                     {
+                        image:"/img/menwear2.webp",
+                        alt="classic shirt",
+                        loading="lazy",
+                        fetchpriority="high"
+                     },
+                     {
+                        image:"/img/menwear3.webp",
+                        alt="classic shirt",
+                        loading="lazy",
+                        fetchpriority="high",
+                        title="I Don't fish for food I fish for vibes"
+                     },
+                     {
+                        image:"/img/menwear4.webp",
+                        alt="classic shirt",
+                        loading="lazy",
+                        fetchpriority="high"
+                        title="I Don't fish for food I fish for vibes"
+                     },
+       ];--*/
+       /*--const imgtag=menImg.filter(item=>item.fetchpriority==="high").map(item=>
+                                    `
+                                      <picture>
+                                            <img src="${item.image} alt="${item.alt}" loading="${item.loading}" fetchpriority="${item.fetchpriority}">
+                                            </picture>
+                                            <h2>${item.title}</h2>
+                                    `).join("");
+       */
+       return`
+            <article>
+               <h1>${title}</h1>
+               <div class="menwear">
+                        
+                        <picture>
+                             <img src="/img/menwear.webp" alt="I don't fish for food i fish for vibes T shirt- by Masterpiece Apparel" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2> I don't fish for food I fish for vibes</h2>
+                                 <p>${creator}</p>
+                                 <button>View Full Details</button> <button class="btnred">Add on Redbubble Cart</button>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/masterpiece-swordfish tshirt.webp" alt="Swordfish Illustration T shirt- by Masterpiece Apparel" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2> Swordfish Illustration T shirt</h2>
+                                 <p>${creator}</p>
+                                 <button>View Full Details</button> <button class="btnred">Add on Redbubble Cart</button>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/masterpiece-DeepSeaMonster.webp" alt="Deep Sea Monster T shirt- by Masterpiece Apparel" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2> Deep Sea Monster T shirt</h2>
+                                 <p>${creator}</p>
+                                 <button>View Full Details</button> <button class="btnred">Add on Redbubble Cart</button>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/masterpiece-SupportMyShop.webp" alt="Support My Shop T shirt- by Masterpiece Apparel" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2>Support My Shop T shirt</h2>
+                                 <p>${creator}</p>
+                                 <button>View Full Details</button> <button class="btnred">Add on Redbubble Cart</button>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/masterpiece-trailwalker.webp" alt="TrailwalkerT shirt- by Masterpiece Apparel" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2>Support Trailwalker T shirt</h2>
+                                 <p>${creator}</p>
+                                 <button>View Full Details</button> <button class="btnred">Add on Redbubble Cart</button>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/masterpiece-lost in space no3.webp" alt="lost in space no.3 T shirt- by Masterpiece Apparel" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2>Support Lost in Space no.3 T shirt</h2>
+                                 <p>${creator}</p>
+                                 <button>View Full Details</button> <button class="btnred">Add on Redbubble Cart</button>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/masterpice-quintessa.webp" alt="Quintessa T shirt- by Masterpiece Apparel" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2>Quintessa T shirt</h2>
+                                 <p>${creator}</p>
+                                 <button>View Full Details</button> <button class="btnred">Add on Redbubble Cart</button>
+                             </div>
+                        </picture>
+
+                        <picture>
+                             <img src="/img/masterpiece-gildark.webp" alt="Gildark T shirt- by Masterpiece Apparel" loading="lazy" fetchpriority="high"/>
+                             <div class="menwear-card">
+                                 <h2>Gildark T shirt</h2>
+                                 <p>${creator}</p>
+                                 <button>View Full Details</button> <button class="btnred">Add on Redbubble Cart</button>
+                             </div>
+                        </picture>
+               </div>
+            </article> 
+            `;
+    }
+    return "";
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("/about.html")){
+
+    }
+    return"";
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("/shop.html")){
+    }
+    return"";
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("/service.html")){
+    }
+    return"";
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("/blog.html")){
+    }
+    return"";
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("/contact.html")){
+    }
+    return"";
+}
---
+++ public/js/main.js
@@ -0,0 +1,45 @@
+export function exp_main(){
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("index.html")){
+        const main_hero='Get our new shirt release and buy now on masterpiece apparel and enjoy discount.';
+        return`
+              <main>
+              <div>
+                   <ul>
+                      <li>July 14, 2026</li>
+                      <li>New shirt release has officially landed today!</li>
+                      <li>Masterpiece-Deep Sea Monster (Essential Shirt)</li>
+                      <li>Only at $30.69</li>
+                      <li>Enjoy Discount 20%-30% Off valid until July 15, 2026</li>
+                      <li>Order and Buy now at Redbubble Store</li>
+                      <li>Follow us on Facebook and Instagram -Facebook: facebook.com/masterpieceapparelbyjoseph  -Instagram: instagram.com/masterpieceapparelbyjoseph</li>
+                      
+                      <li>July 14, 2026</li>
+                      <li>New shirt release has officially landed today!</li>
+                      <li>Masterpiece- Swordfish T Shirt (Essential Shirt)</li>
+                      <li>Only at $30.69</li>
+                      <li>Enjoy Discount 20%-30% Off valid until July 15, 2026</li>
+                      <li>Order and Buy now at Redbubble Store</li>
+                      <li>Follow us on Facebook and Instagram -Facebook: facebook.com/masterpieceapparelbyjoseph  -Instagram: instagram.com/masterpieceapparelbyjoseph</li>
+                   </ul> 
+              </div>
+              <h1>${main_hero}</h1>
+              </main>
+        `;
+    }
+    return "";
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("/about.html")){
+    }
+    return"";
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("/shop.html")){
+    }
+    return"";
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("/service.html")){
+    }
+    return"";
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("/blog.html")){
+    }
+    return"";
+    if (window.location.pathname==="/" || window.location.pathname.endsWith("/contact.html")){
+    }
+    return"";
+}
