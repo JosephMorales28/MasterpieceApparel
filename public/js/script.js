7b6e9e0fd7295980d239726a5f34174e1a1b03d2
JosephMorales28 <josephmo2893@gmail.com>
2026-07-25 14:54:01 +0800

    update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ import {exp_article} from './article.js?v=20260716';
 import {exp_aside} from './aside.js?v=20260716';
 import {exp_footer} from './footer.js?v=20260716';
 
-const supportedPages=['about','shop','service','blog','contact'];
+const supportedPages=['about','shop','service','blog','contact','idontfishforfoodifishforvibes'];
 const lastPathSegment=window.location.pathname.split('/').filter(Boolean).pop() || 'home';
 const pageName=lastPathSegment.replace(/\.html$/, '');
 const page=pageName==='index' || pageName==='home' ? 'home' : supportedPages.includes(pageName) ? pageName : 'home';
