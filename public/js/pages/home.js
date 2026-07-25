f1bc83cc1f79f3b226961a49a97b26f1ef60a864
JosephMorales28 <josephmo2893@gmail.com>
2026-07-25 18:25:19 +0800

    to be continue to fix organize structure in javascript by using routes.js in per pages

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -1,3 +1,23 @@
 export function homePage(){
-    return``;
+            
+    const mainHero = 'Get our new shirt release and buy now on Masterpiece Apparel and enjoy a discount.';
+    const mainlist=[
+                            'July 14, 2026',
+                            'New shirt release has officially landed today!',
+                            'Masterpiece-Deep Sea Monster (Essential Shirt)',
+                            'Only at $30.69',
+                            'Enjoy Discount 20%-30% Off valid until July 15, 2026',
+                            'Order and Buy now at Redbubble Store',
+                            'Follow us on Facebook and Instagram - Facebook: facebook.com/masterpieceapparelbyjoseph - Instagram: instagram.com/masterpieceapparelbyjoseph'
+                    ];
+    return `
+            <main>
+                <div>
+                    <ul>
+                        ${mainlist.map(mainlisting=>`<li>${mainlisting}</li>`).join('')}
+                    </ul>
+                </div>
+                <h1>${mainHero}</h1>
+            </main>
+        `;
 }
