1e14e636863c76270c353baa939f0fdcdc49911e
JosephMorales28 <josephmo2893@gmail.com>
2026-07-29 18:50:55 +0800

    upgrade main page

--- public/js/components/main.js
+++ public/js/components/main.js
@@ -39,8 +39,34 @@ export function exp_main(page) {
             return `
                 <main>
                     <div class="main_hero">
-                       <img src="/img/menwear.webp"/>
-                       <img src="/img/menwear.webp"/>
+                        <div>
+                           <img src="/img/part2.webp"/>
+                           <div class="bottom-left-text">
+                              <p>I don't fish for food I fish for vibes </p>
+                              <span>Essential shirt</span>
+                           </div>
+                        </div>
+                        <div>
+                           <img src="/img/part3.webp"/>
+                           <div class="bottom-left-text">
+                              <p>Lost in Space No.3</p>
+                              <span>Essential shirt</span>
+                           </div>
+                        </div>
+                        <div>
+                           <img src="/img/part4.webp"/>
+                           <div class="bottom-left-text">
+                              <p>Doggo RPG: Fetch of Destiny </p>
+                              <span>Essential shirt</span>
+                           </div>
+                        </div>
+                        <div>
+                           <img src="/img/part5.webp"/>
+                           <div class="bottom-left-text">
+                              <p>Swordfish</p>
+                              <span>Hoodie</span>
+                           </div>
+                        </div>
                     </div>
                 </main>
             `;
