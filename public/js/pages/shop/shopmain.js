5b0cb491d86fbd327de321b98b566be430446e41
JosephMorales28 <josephmo2893@gmail.com>
2026-08-09 16:07:01 +0800

    update shop.html with form

--- public/js/pages/shop/shopmain.js
+++ public/js/pages/shop/shopmain.js
@@ -3,6 +3,10 @@ export function shopMain(){
       <main>
         <div id="shopmain">
         <h1>Masterpiece Apparel Shop</h1>
+        <form>
+           <input type="text" placeholder="Search" id="placeholder">
+           <input type="submit" value="Search">
+        </form>
         </div>
       </main>
     `;
