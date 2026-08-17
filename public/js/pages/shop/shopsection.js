c6b89fa226e383b91f983ff2f55e56b26b76be3c
JosephMorales28 <josephmo2893@gmail.com>
2026-08-17 16:22:38 +0800

    upgrade shop page image

--- public/js/pages/shop/shopsection.js
+++ public/js/pages/shop/shopsection.js
@@ -10,9 +10,11 @@ class ImageProduct{
 
     getImageProduct(){
         return`
-               <img src="${this.image}" alt="${this.alt}" loading= "${this.loading === 0 ? "eager" : "lazy"}" fetchpriority="${this.priority === 0 ? "High" : "Auto"}" decoding="async"/>
-               <p>${this.name}</p>
-               <p>${this.details}</p>
+               <div>
+                   <img src="${this.image}" alt="${this.alt}" width="500" height="500" loading= "${this.loading === 0 ? "eager" : "lazy"}" fetchpriority="${this.priority === 0 ? "High" : "Auto"}" decoding="async"/>
+                   <h1>${this.name}</h1>
+                   <p>${this.details}</p>
+               </div>
               `
     }
 }
@@ -21,7 +23,7 @@ export function shopSection(search=""){
 
     const image_product=[ 
         new ImageProduct(
-                "#",
+                "./img/idontfish.avif",
                 "i don't fish for food i fish for vibes",
                 0,
                 0,
@@ -29,12 +31,28 @@ export function shopSection(search=""){
                 "Bring adventure and humor to your wardrobe with this anime‑style fluffy cat fishing scene! Featuring a beige‑brown long‑haired cat in a blue fishing coat and sunglasses, sitting on a folding chair by the pond with rod in paw. To the left is a tackle table, to the right a blue cooler bag filled with freshly caught fish, and behind the cat sits a cozy camping car — the perfect outdoor setup."
         ),
         new ImageProduct(
-                "#",
+                "./img/Deep Sea Monster.avif",
                 "Deep Sea Monster",
                 0,
                 0,
                 "Deep Sea Monster",
                 "Dive into the abyss with this Deep Sea Monster artwork a colossal Kraken rising from storm‑tossed waves. Its glowing eyes, razor‑sharp teeth, and curling tentacles crush a doomed ship beneath a fiery moon."
+        ),
+        new ImageProduct(
+                "./img/Celestial hare.avif",
+                "Celestial Hare Chaos",
+                0,
+                0,
+                "Celestial Hare Chaos",
+                "Step into the arena with this electrifying rabbit mage hero! ⚡? Floating mid-air with a crazed grin, this full-body design captures the thrill of magical combat. Dressed in crosshair-patterned wizard armor and wielding a glowing staff, the rabbit radiates tournament energy — a champion ready to unleash chaos."
+        ),
+        new ImageProduct(
+                "./img/idontfish.avif",
+                "i don't fish for food i fish for vibes",
+                0,
+                0,
+                "i don't fish for food i fish for vibes.",
+                "Bring adventure and humor to your wardrobe with this anime‑style fluffy cat fishing scene! Featuring a beige‑brown long‑haired cat in a blue fishing coat and sunglasses, sitting on a folding chair by the pond with rod in paw. To the left is a tackle table, to the right a blue cooler bag filled with freshly caught fish, and behind the cat sits a cozy camping car — the perfect outdoor setup."
         )
     ]
     
@@ -45,7 +63,9 @@ export function shopSection(search=""){
 
     return`
           <section>
-              ${shopHTML}
+             <div id="shopsection">
+                ${shopHTML}
+            </div>
           </section>
     `;
 }
