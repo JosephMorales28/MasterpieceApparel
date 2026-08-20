9102e1a89398cea8719c34208d41fd203e5d67b1
JosephMorales28 <josephmo2893@gmail.com>
2026-08-20 16:26:36 +0800

    add new product image on shop

--- public/js/pages/shop/shopsection.js
+++ public/js/pages/shop/shopsection.js
@@ -33,25 +33,41 @@ export function shopSection(search=""){
         new ImageProduct(
                 "./img/Deep Sea Monster.avif",
                 "Deep Sea Monster",
-                0,
-                0,
+                1,
+                1,
                 "Deep Sea Monster",
                 "Dive into the abyss with this Deep Sea Monster artwork a colossal Kraken rising from storm‑tossed waves. Its glowing eyes, razor‑sharp teeth, and curling tentacles crush a doomed ship beneath a fiery moon."
         ),
         new ImageProduct(
                 "./img/Celestial hare.avif",
                 "Celestial Hare Chaos",
-                0,
-                0,
+                1,
+                1,
                 "Celestial Hare Chaos",
                 "Step into the arena with this electrifying rabbit mage hero! ⚡? Floating mid-air with a crazed grin, this full-body design captures the thrill of magical combat. Dressed in crosshair-patterned wizard armor and wielding a glowing staff, the rabbit radiates tournament energy — a champion ready to unleash chaos."
         ),
         new ImageProduct(
-                "./img/idontfish.avif",
-                "i don't fish for food i fish for vibes",
-                0,
-                0,
-                "i don't fish for food i fish for vibes.",
+                "./img/Shockwave Striker.webp",
+                "Shockwave Striker",
+                1,
+                1,
+                "Shockwave Striker",
+                "Bring adventure and humor to your wardrobe with this anime‑style fluffy cat fishing scene! Featuring a beige‑brown long‑haired cat in a blue fishing coat and sunglasses, sitting on a folding chair by the pond with rod in paw. To the left is a tackle table, to the right a blue cooler bag filled with freshly caught fish, and behind the cat sits a cozy camping car — the perfect outdoor setup."
+        ),
+        new ImageProduct(
+                "./img/Goal Bunny Frenzy Global Soccer Edition.webp",
+                "Goal Bunny Frenzy Global Soccer Edition",
+                1,
+                1,
+                "Goal Bunny Frenzy Global Soccer Edition",
+                "Step into the arena with this electrifying rabbit mage hero! ⚡? Floating mid-air with a crazed grin, this full-body design captures the thrill of magical combat. Dressed in crosshair-patterned wizard armor and wielding a glowing staff, the rabbit radiates tournament energy — a champion ready to unleash chaos."
+        ),
+        new ImageProduct(
+                "./img/Goal Keeper Gone Bananas Monkey Soccer Madness.webp",
+                "Goal Keeper Gone Bananas Monkey Soccer Madness",
+                1,
+                1,
+                "Goal Keeper Gone Bananas Monkey Soccer Madness",
                 "Bring adventure and humor to your wardrobe with this anime‑style fluffy cat fishing scene! Featuring a beige‑brown long‑haired cat in a blue fishing coat and sunglasses, sitting on a folding chair by the pond with rod in paw. To the left is a tackle table, to the right a blue cooler bag filled with freshly caught fish, and behind the cat sits a cozy camping car — the perfect outdoor setup."
         )
     ]
